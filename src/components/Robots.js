451f07a2a172b5ff3f91a932dcdca461801c76b3
AdrianMAD <46680955+AdrianMAD@users.noreply.github.com>
2019-02-05 14:29:13 +0200

    adding starting project

---
+++ src/components/Robots.js
@@ -0,0 +1,48 @@
+export const Robots = [
+{
+	id: 0,
+	name:"Mildred O'Gallagher",
+	username:"flatfoot",
+	email:"drolsky@mac.com"
+},
+{	id: 1,
+	name:"Jeremy Mccullough",
+	username:"sophi",
+	email:"geoffr@outlook.com"
+},
+{	id: 2,
+	name:"Tyreese Andrews",
+	username:"essential",
+	email:"padme@optonline.net"
+},
+{	id: 3,
+	name:"Mikail Aguirre",
+	username:"enuciate",
+	email:"melnik@att.net"
+},
+{
+	id: 4,
+	name:"Alannah Stanley",
+	username:"caundle",
+	email:"iapetus@icloud.com"
+},
+{
+	id: 5,
+	name:"Susannah Miles",
+	username:"directory",
+	email:"cantu@outlook.com"
+},
+{
+	id: 6,
+	name:"Ava-Grace Keith",
+	username:"rusty",
+	email:"lbecchi@verizon.net"
+},
+{
+	id: 7,
+	name:"Tallulah Maxwell",
+	username:"fixation",
+	email:"empathy@mac.com"
+}
+
+];
---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList';
+import {Robots} from '../components/Robots' 
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+
+class App extends Component {
+	constructor () {
+		super ()
+		this.state = {
+			Robots: [],
+			searchfield:''
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response=> {return response.json()})
+			.then(users=> {this.setState({ Robots:users})
+			})
+	}
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value })
+	}
+	render () {
+		const filteredRobot = this.state.Robots.filter(Robots =>{
+		return Robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+			if (!this.state.Robots.length) {
+			return <h1>The robots are stuck in traffic</h1>
+			} else {
+				return(
+					<div className ="tc">
+						<h1 className="f1">RoboFriends</h1>
+						<SearchBox searchChange ={this.onSearchChange}/>
+						<Scroll>
+							<CardList Robots={filteredRobot}/>
+						</Scroll>
+					</div>
+				)
+			}
+}
+}
+
+export default App;
